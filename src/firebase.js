434e0cb24458efa1193a0c57997a1579fbad662b
Manel Morales Rivera <manelmoralesrivera@gmail.com>
2025-04-24 23:12:23 +0200

    Inicialización del proyecto de DAW basandonos en las tecnologías que estoy aprendiendo en la empresa (React, MaterialUI y Firebase), también utilizamos Vite para crear un proyecto con una template de React.

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBd7dr5WPiFGLdLl9ToVc5g7a45kt3omYM",
+    authDomain: "petmatch-cf175.firebaseapp.com",
+    projectId: "petmatch-cf175",
+    storageBucket: "petmatch-cf175.firebasestorage.app",
+    messagingSenderId: "554184011415",
+    appId: "1:554184011415:web:d1c9203eed80fd917503d2"
+};
+  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
